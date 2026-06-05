7c23356d108335832a59e2f95459e3daf7bcd232
Rasmus Anthin <scratty@gmail.com>
2026-06-05 19:58:12 +0200

    Adding main.cpp (vibed with codex).

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "cli.h"
+
+#include <iostream>
+#include <string_view>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+  std::vector<std::string_view> arguments;
+  arguments.reserve(static_cast<std::size_t>(argc > 0 ? argc - 1 : 0));
+
+  for (int index = 1; index < argc; ++index)
+  {
+    arguments.emplace_back(argv[index]);
+  }
+
+  return forge::cli::run(arguments, std::cout, std::cerr);
+}
